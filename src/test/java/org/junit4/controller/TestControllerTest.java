c5fe346edefdad05566b92c1bd3167eea4e5b578
zhangchao <zhangchao@ahjkii.com>
2020-08-26 16:51:08 +0800

    init

---
+++ src/test/java/org/junit4/controller/TestControllerTest.java
@@ -0,0 +1,22 @@
+package org.junit4.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void hello() {
+    }
+}
